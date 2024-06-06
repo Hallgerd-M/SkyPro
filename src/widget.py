22d2806c341bac02e9100d6a7de86e29c8d981ef
Irina Polevaya <mirwen@yandex.ru>
2024-06-07 01:25:42 +0300

    mask_account_card second part

--- src/widget.py
+++ src/widget.py
@@ -1,4 +1,5 @@
-from typing import Union
+
+
 
 def mask_account_card(card_or_account_info: str) -> str:
     """принимает название и номер карты или номер счета и возвращает замаскированный номер"""
@@ -11,5 +12,12 @@ def mask_account_card(card_or_account_info: str) -> str:
             mask_account_number = get_mask_account(account_number)
             account_info.append(mask_account_number)
             mask_account_info = " ".join(account_info)
-    return mask_account_info
+            print(mask_account_info)
+        elif letter == "M" or letter == "V":
+            card_info.append(card_or_account_info[:-17])
+            card_number = int(card_or_account_info[-16:])
+            mask_card_number = get_mask_card_number(card_number)
+            card_info.append(mask_card_number)
+            mask_card_info = " ".join(card_info)
+            print(mask_card_info)
 
