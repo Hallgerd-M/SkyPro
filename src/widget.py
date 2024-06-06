44b858522e164bbdac22edd3ffc3fd5c9d8874eb
Irina Polevaya <mirwen@yandex.ru>
2024-06-07 01:33:54 +0300

    def get_data added

--- src/widget.py
+++ src/widget.py
@@ -12,12 +12,19 @@ def mask_account_card(card_or_account_info: str) -> str:
             mask_account_number = get_mask_account(account_number)
             account_info.append(mask_account_number)
             mask_account_info = " ".join(account_info)
-            print(mask_account_info)
+            return mask_account_info
         elif letter == "M" or letter == "V":
             card_info.append(card_or_account_info[:-17])
             card_number = int(card_or_account_info[-16:])
             mask_card_number = get_mask_card_number(card_number)
             card_info.append(mask_card_number)
             mask_card_info = " ".join(card_info)
-            print(mask_card_info)
+            return mask_card_info
+
+
+def get_data(date_info: str) -> str:
+    date_data = []
+    date_data.extend([date_info[8:10], date_info[5:7], date_info[:4]])
+    new_date_data = ".".join(date_data)
+    return new_date_data
 
