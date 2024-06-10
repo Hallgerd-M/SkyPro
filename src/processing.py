5907353c0a330fbb87bd52d87b4a1d73ce963a59
Irina Polevaya <mirwen@yandex.ru>
2024-06-10 16:52:29 +0300

    modified and checked functions

--- src/processing.py
+++ src/processing.py
@@ -1,26 +1,70 @@
-def filter_by_state(my_dictionary: dict, state = 'EXECUTED') -> dict:
+from typing import List, bool
+
+def filter_by_state(my_dictionary: list, state: str ='EXECUTED') -> list:
     """принимает список словарей и возвращает новый список по значению ключа state"""
     new_dictionary = []
     for item in my_dictionary:
-        if item['state'] == state:
+        if item["state"] == state:
             new_dictionary.append(item)
     return new_dictionary
 
 
-def sort_by_date(my_dictionary: dict) -> dict:
+def sort_by_date(my_dictionary: list, reverse: bool = True) -> list:
     """сортирует принимаемый список по дате"""
-    sorted_dictionary = sorted(my_dictionary, key=lambda item:item['date'])
+    sorted_dictionary = sorted(my_dictionary, key=lambda item: item["date"])
     return sorted_dictionary
 
 
-
-
 if __name__ == "__main__":
-    print(filter_by_state([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-                           {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-                           {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-                           {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]))
-    print(sort_by_date([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-                           {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-                           {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-                           {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]))
+    print(
+        filter_by_state(
+            [
+                {
+                    "id": 41428829,
+                    "state": "EXECUTED",
+                    "date": "2019-07-03T18:35:29.512364",
+                },
+                {
+                    "id": 939719570,
+                    "state": "EXECUTED",
+                    "date": "2018-06-30T02:08:58.425572",
+                },
+                {
+                    "id": 594226727,
+                    "state": "CANCELED",
+                    "date": "2018-09-12T21:27:25.241689",
+                },
+                {
+                    "id": 615064591,
+                    "state": "CANCELED",
+                    "date": "2018-10-14T08:21:33.419441",
+                },
+            ]
+        )
+    )
+    print(
+        sort_by_date(
+            [
+                {
+                    "id": 41428829,
+                    "state": "EXECUTED",
+                    "date": "2019-07-03T18:35:29.512364",
+                },
+                {
+                    "id": 939719570,
+                    "state": "EXECUTED",
+                    "date": "2018-06-30T02:08:58.425572",
+                },
+                {
+                    "id": 594226727,
+                    "state": "CANCELED",
+                    "date": "2018-09-12T21:27:25.241689",
+                },
+                {
+                    "id": 615064591,
+                    "state": "CANCELED",
+                    "date": "2018-10-14T08:21:33.419441",
+                },
+            ],
+        )
+    )
