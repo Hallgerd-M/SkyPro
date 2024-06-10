5e17494c36f9888bdfceb74ee7e91ffdf2832155
Irina Polevaya <mirwen@yandex.ru>
2024-06-10 16:31:13 +0300

    added sort_by_date function

--- src/processing.py
+++ src/processing.py
@@ -1,6 +1,4 @@
-from typing import Dict
-
-def filter_by_state(my_dictionary: dict, state: str) -> dict:
+def filter_by_state(my_dictionary: dict, state = 'EXECUTED') -> dict:
     """принимает список словарей и возвращает новый список по значению ключа state"""
     new_dictionary = []
     for item in my_dictionary:
@@ -9,8 +7,20 @@ def filter_by_state(my_dictionary: dict, state: str) -> dict:
     return new_dictionary
 
 
-def sort_by_date():
-    pass
+def sort_by_date(my_dictionary: dict) -> dict:
+    """сортирует принимаемый список по дате"""
+    sorted_dictionary = sorted(my_dictionary, key=lambda item:item['date'])
+    return sorted_dictionary
+
+
+
 
 if __name__ == "__main__":
-    print(filter_by_state([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}, {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}, {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}], 'EXECUTED'))
+    print(filter_by_state([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+                           {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+                           {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+                           {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]))
+    print(sort_by_date([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+                           {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+                           {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+                           {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]))
--- src/widget.py
+++ src/widget.py
@@ -1,30 +1,28 @@
+from datetime import datetime
 
+from src.masks import get_mask_account, get_mask_card_number
 
 
 def mask_account_card(card_or_account_info: str) -> str:
     """принимает название и номер карты или номер счета и возвращает замаскированный номер"""
-    card_info = []
-    account_info = []
-    for letter in card_or_account_info:
-        if letter == "С":
-            account_info.append(card_or_account_info[:4])
-            account_number = int(card_or_account_info[5:])
-            mask_account_number = get_mask_account(account_number)
-            account_info.append(mask_account_number)
-            mask_account_info = " ".join(account_info)
-            return mask_account_info
-        elif letter == "M" or letter == "V":
-            card_info.append(card_or_account_info[:-17])
-            card_number = int(card_or_account_info[-16:])
-            mask_card_number = get_mask_card_number(card_number)
-            card_info.append(mask_card_number)
-            mask_card_info = " ".join(card_info)
-            return mask_card_info
+    payment_info = card_or_account_info.split(" ")
+    payment_number = int(payment_info[-1])
+    payment_name = payment_info[:-1]
+    if card_or_account_info.lower().startswith("счет"):
+        number = get_mask_account(payment_number)
+    else:
+        number = get_mask_card_number(payment_number)
+    return " ".join(payment_name) + f" {number}"
 
 
-def get_data(date_info: str) -> str:
-    date_data = []
-    date_data.extend([date_info[8:10], date_info[5:7], date_info[:4]])
-    new_date_data = ".".join(date_data)
-    return new_date_data
+def get_date(date_info: str) -> str:
+    """преобразует дату в формат dd.mm.yy"""
+    converted_date = datetime.fromisoformat(date_info).strftime("%d.%m.%Y")
+    return converted_date
 
+
+if __name__ == "__main__":
+    print(mask_account_card("Maestro 1596837868705199"))
+    print(mask_account_card("Счет 64686473678894779589"))
+    print(mask_account_card("Visa Classic 6831982476737658"))
+    print(get_date("2018-07-11T02:26:18.671407"))
