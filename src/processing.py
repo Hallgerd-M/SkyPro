a7267b44231b876e112673ef0c62f12b6c82152f
Irina Polevaya <mirwen@yandex.ru>
2024-06-13 15:27:00 +0300

    added tests

--- src/processing.py
+++ src/processing.py
@@ -1,4 +1,4 @@
-from typing import List, bool
+from typing import List
 
 def filter_by_state(my_dictionary: list, state: str ='EXECUTED') -> list:
     """принимает список словарей и возвращает новый список по значению ключа state"""
@@ -11,7 +11,10 @@ def filter_by_state(my_dictionary: list, state: str ='EXECUTED') -> list:
 
 def sort_by_date(my_dictionary: list, reverse: bool = True) -> list:
     """сортирует принимаемый список по дате"""
-    sorted_dictionary = sorted(my_dictionary, key=lambda item: item["date"])
+    if reverse is True:
+        sorted_dictionary = sorted(my_dictionary, key=lambda item: item["date"])
+    else:
+        sorted_dictionary = sorted(my_dictionary, key=lambda item: item["date"], reverse=True)
     return sorted_dictionary
 
 
@@ -65,6 +68,28 @@ if __name__ == "__main__":
                     "state": "CANCELED",
                     "date": "2018-10-14T08:21:33.419441",
                 },
-            ],
-        )
+            ])
     )
+
+
+
+"""    assert sort_by_date(my_dictionary) == [{
+                    "id": 939719570,
+                    "state": "EXECUTED",
+                    "date": "2018-06-30T02:08:58.425572",
+                },
+                {
+                    "id": 594226727,
+                    "state": "CANCELED",
+                    "date": "2018-09-12T21:27:25.241689",
+                },
+                {
+                    "id": 615064591,
+                    "state": "CANCELED",
+                    "date": "2018-10-14T08:21:33.419441",
+                },
+                {
+                    "id": 41428829,
+                    "state": "EXECUTED",
+                    "date": "2019-07-03T18:35:29.512364",
+                }]"""
