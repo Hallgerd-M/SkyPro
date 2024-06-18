94124f5a1e198424e02b2ba6c8457f21b5fa142e
Irina Polevaya <mirwen@yandex.ru>
2024-06-19 02:40:26 +0300

    deorator function partly written

---
+++ src/decorators.py
@@ -0,0 +1,26 @@
+from typing import Any, Callable
+
+def log(filename:Any):
+    """ Логирует вызов функции и ее результат в файл или в консоль"""
+    def decorator(func: Callable):
+        def wrapper(*args, **kwargs):
+            result = func(*args, **kwargs)
+            print(result)
+            if result == sum(args):
+                # filename.write("my_function ok")
+                print("my_function ok")
+            elif no result:
+                raise ValueError
+                error = "ValueError"
+                print(f"my_function error: {error}. Inputs:{args}, {kwargs}")
+                #filename.write(f"my_function error: {error}. Inputs:{args}, {kwargs}")
+            return result
+
+        return wrapper
+    return decorator
+
+@log(filename="mylog.txt")
+def my_function(x, y):
+    return x / y
+
+my_function(2, 0)
