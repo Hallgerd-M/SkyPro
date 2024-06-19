9a07189e3f1ae88b28891953c9154a23e0cd5df7
Irina Polevaya <mirwen@yandex.ru>
2024-06-19 12:32:17 +0300

    tests for decorators modified

--- src/decorators.py
+++ src/decorators.py
@@ -1,15 +1,17 @@
 from typing import Any, Callable
+from functools import wraps
 
 def log(filename:Any):
     """ Логирует вызов функции и ее результат в файл или в консоль"""
     def decorator(func: Callable):
+        @wraps(func)
         def wrapper(*args, **kwargs):
             result = func(*args, **kwargs)
             print(result)
             if result == sum(args):
                 # filename.write("my_function ok")
                 print("my_function ok")
-            elif no result:
+            else:
                 raise ValueError
                 error = "ValueError"
                 print(f"my_function error: {error}. Inputs:{args}, {kwargs}")
@@ -21,6 +23,6 @@ def log(filename:Any):
 
 @log(filename="mylog.txt")
 def my_function(x, y):
-    return x / y
+    return x + y
 
-my_function(2, 0)
+my_function(2, 8)
---
+++ tests/test_decorators.py
@@ -0,0 +1,26 @@
+import pytest
+from src.decorators import log, my_function
+
+def test_log(capsys):
+    with pytest.raises(Exception, match = "Error"):
+        captured = capsys.readouterr()
+        assert captured.out == match
+    @log
+    def my_function(x = 2, y = 3):
+        return x + y
+
+
+
+"""
+    result = my_function(2, 3)
+   
+
+
+def test_retry_decorator():
+    with pytest.raises(Exception, match="Max retries exceeded"):
+        example_function()
+
+        hello_world()
+        captured = capsys.readouterr()
+        assert captured.out == "Hello, world!\n"
+"""
