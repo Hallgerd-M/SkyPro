23d65ef3ebd9818385bb6c77bc364a65844ab231
Irina Polevaya <mirwen@yandex.ru>
2024-07-04 02:19:16 +0300

    tests modified

--- src/utils.py
+++ src/utils.py
@@ -35,26 +35,28 @@ def get_transactions_dictionary(path: str) -> Any:
 
 def return_transaction_amount_in_rub(transactions: list, transaction_id: int) -> Any:
     """Функция принимает транзакцию и возвращает сумму транзакции в рублях, если не в рублях, конвертирует в рубли"""
+    logger.info("Getting operation amount starts")
     for transaction in transactions:
-        try:
-            if transaction["id"] == transaction_id:
-                logger.info("Getting operation amount starts")
-                if transaction["operationAmount"]["currency"]["code"] == "RUB":
-                    rub_amount = transaction["operationAmount"]["amount"]
+        if transaction.get("id") == transaction_id:
+            if transaction["operationAmount"]["currency"]["code"] == "RUB":
+                rub_amount = transaction["operationAmount"]["amount"]
+                logger.info(f"Operation amount in RUB:{rub_amount}")
+                return rub_amount
+            else:
+                not_rub_amount = transaction["operationAmount"]["amount"]
+                logger.info(f"Operation amount in USD/EUR:{not_rub_amount}")
+                currency = transaction["operationAmount"]["currency"]["code"]
+                rub_amount = round(convert_to_rub(not_rub_amount, currency), 2)
+                if rub_amount != 0:
                     logger.info(f"Operation amount in RUB:{rub_amount}")
                     return rub_amount
                 else:
-                    not_rub_amount = transaction["operationAmount"]["amount"]
-                    logger.info(f"Operation amount in USD/EUR:{not_rub_amount}")
-                    currency = transaction["operationAmount"]["currency"]["code"]
-                    rub_amount = round(convert_to_rub(not_rub_amount, currency), 2)
-                    logger.info(f"Operation amount in RUB:{rub_amount}")
-                    return rub_amount
-        except FileNotFoundError:
-            logger.error("Transaction not found")
-            return "Транзакция не найдена"
-
+                    logger.error("Operation amount can't be converted to RUB")
+                    return "Конвертация не может быть выполнена"
+    else:
+        logger.error("Transaction not found")
+        return "Транзакция не найдена"
 
 if __name__ == "__main__":
     transactions = get_transactions_dictionary("../data/operations.json")
-    print(return_transaction_amount_in_rub(transactions, 441945886))
+    print(return_transaction_amount_in_rub(transactions, 41428829))
